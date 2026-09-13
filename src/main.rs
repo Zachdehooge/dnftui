06ea89e955cde52f0e43170d4372ff1616cdbc93
zachdehooge <zacharydehooge@gmail.com>
2026-09-13 15:21:09 -0400

    User Input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
+use std::io;
+
 fn search_dnf(repo: &str) -> String{
     let command = format!("dnf search {repo}");
     command
 }
 
 fn main() {
-    println!("{}", search_dnf("vscode"));
+    let mut input = String::new();
+
+    println!("Enter a repo:");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let name = input.trim();
+
+    println!("{}", search_dnf(&name));
 }
